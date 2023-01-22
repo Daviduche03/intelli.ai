8e861250839112f4d3583e81e3ad23e52f9b7da8
daviduche03 <daviduche4809@gmail.com>
2023-01-22 15:02:53 +0100

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+ CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
